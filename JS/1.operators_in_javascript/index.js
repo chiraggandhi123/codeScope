1951de2bd83e0d229f3057acd7b15f3eaa127075
chiraggandhi123 <chiraggandhi70726@gmail.com>
2024-02-04 18:01:21 +0530

    feat: added operators in js

---
+++ JS/1.operators_in_javascript/index.js
@@ -0,0 +1,15 @@
+let a = 3;
+let b = 2;
+
+// console.log("a" + " + " + "b", a+b);
+// console.log("a" + " - " + "b", a-b);
+// console.log("a" + " * " + "b", a*b);
+// console.log("a" + " / " + "b", a/b);
+// console.log("a" + " ** " + "b", a**b);
+// console.log("a" + " % " + "b", a%b);
+// console.log("a" + "++ " , a++);
+// console.log("a", a);
+// console.log("a" + "-- " , a--);
+// console.log("a", a);
+
+
