7be16da1b05d7639adde7a2b9fa33b25473d5da4
chiraggandhi123 <chiraggandhi70726@gmail.com>
2024-02-09 09:29:16 +0530

    feat: added new GTO

---
+++ problems/GTO-1/index.js
@@ -0,0 +1,10 @@
+for(var i = 0; i< 3;i++){
+    setTimeout(()=>console.log(i),1000)
+}
+for(let i = 0; i< 3;i++){
+    setTimeout(()=>console.log(i),1000)
+}
+
+//A : 0 1 2 and 0 1 2
+//B : 0 1 2 and 3 3 3
+//C : 3 3 3 and 0 1 2 
