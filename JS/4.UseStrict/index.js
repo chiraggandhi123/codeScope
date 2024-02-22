317f2f1812177789ccbb7f2e74fa2d349d4e0db7
chiraggandhi123 <chiraggandhi70726@gmail.com>
2024-02-22 17:16:38 +0530

    feat: added some content for videos

---
+++ JS/4.UseStrict/index.js
@@ -0,0 +1,11 @@
+//strict mode ON
+"use strict";
+
+
+
+
+
+
+
+
+
--- problems/GTO-1/index.js
+++ problems/GTO-1/index.js
@@ -1,9 +1,9 @@
-for(var i = 0; i< 3;i++){
+for(var i = 0; i< 1000;i++){
     setTimeout(()=>console.log(i),1000)
 }
-for(let i = 0; i< 3;i++){
-    setTimeout(()=>console.log(i),1000)
-}
+// for(let i = 0; i< 1000;i++){
+//     setTimeout(()=>console.log(i),1000)
+// }
 
 //A : 0 1 2 and 0 1 2
 //B : 0 1 2 and 3 3 3
